03b4363092b771cc6bc1c61e39ea41fbdd1b7fdf
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-11-16 09:52:46 -0800

    finished demo v1

---
+++ queue.js
@@ -0,0 +1,64 @@
+function queue() {
+  //Challenge no use of array methods
+  let queue = [];
+
+  function enqueue(newItem) {
+    // queue = [newItem, ...queue];
+
+    const newArray = [];
+
+    for (let i = 0; i <= queue.length; i++) {
+      if (i === 0) {
+        newArray[i] = newItem;
+        continue;
+      }
+
+      //copy queue but with all items shifted up one.
+      newArray[i] = queue[i - 1];
+    }
+    queue = newArray;
+  }
+
+  function dequeue() {
+    if (queue.length === 0) {
+      console.error("queue is empty");
+      return;
+    }
+    const newArray = [];
+
+    //store last item to be returned later
+    let dequeuedItem = queue[queue.length - 1];
+
+    //copy existing queue except for last item
+    for (let i = 0; i < queue.length - 1; i++) {
+      newArray[i] = queue[i];
+    }
+
+    //create new queue without one less item in it.
+    queue = newArray;
+
+    return dequeuedItem;
+  }
+
+  function print() {
+    console.log("queue", queue);
+  }
+
+  return {
+    enqueue,
+    dequeue,
+    print,
+  };
+}
+
+const myQueue = queue();
+
+myQueue.print();
+myQueue.dequeue();
+myQueue.enqueue("foo");
+myQueue.enqueue("bar");
+myQueue.print();
+console.log("dequeue", myQueue.dequeue());
+myQueue.print();
+console.log("dequeue", myQueue.dequeue());
+myQueue.print();
